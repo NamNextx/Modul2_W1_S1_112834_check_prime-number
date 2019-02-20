2a3d88de81f9db3868d6e70db2a7ef396775be3f
Namnextx <phuongnam.nextx@gmail.com>
2019-02-20 09:39:41 +0700

    done

---
+++ src/Checksonguyento.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Checksonguyento {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Pls input your number");
+        int number=sc.nextInt();
+        if (checkPrimeNumber(number)){
+            System.out.println("Is not a prime");
+        }
+        else System.out.println("Number "+number+" is a prime number");
+    }
+
+    static boolean checkPrimeNumber(int number){
+        boolean flag=false;
+        if (number<2){
+            flag=true;
+        }
+        else {
+            int i=2;
+            while (i<=Math.sqrt(number)){
+                if (number%i==0){
+                    flag=true;
+                    break;
+
+                }
+                i++;
+            }
+
+        }
+        return flag;
+    }
+}
